f33809e418601362b54cae6a72f11c1bf2b9c324
SamuelHGerber <henrylightfoot@gmail.com>
2021-02-19 15:16:01 -0800

    endpoint 1 /planets passes test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,53 +9,80 @@ const client = require('../lib/client');
 describe('app routes', () => {
   describe('routes', () => {
     let token;
-  
+
     beforeAll(async done => {
       execSync('npm run setup-db');
-  
+
       client.connect();
-  
+
       const signInData = await fakeRequest(app)
         .post('/auth/signup')
         .send({
           email: 'jon@user.com',
           password: '1234'
         });
-      
+
       token = signInData.body.token; // eslint-disable-line
-  
+
       return done();
     });
-  
+
     afterAll(done => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('returns planets', async () => {
 
       const expectation = [
         {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
+          "id": 1,
+          "planet": "mercury",
+          "class": "terrestrial",
+          "diameter": 4880,
+          "gravity": "0.4",
+          "magnetic_field_strong": false,
+          "owner_id": 1
         },
         {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
+          "id": 2,
+          "planet": "venus",
+          "class": "terrestrial",
+          "diameter": 12102,
+          "gravity": "0.9",
+          "magnetic_field_strong": false,
+          "owner_id": 1
         },
         {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
+          "id": 3,
+          "planet": "earth",
+          "class": "terrestrial",
+          "diameter": 12742,
+          "gravity": "1",
+          "magnetic_field_strong": true,
+          "owner_id": 1
+        },
+        {
+          "id": 4,
+          "planet": "mars",
+          "class": "terrestrial",
+          "diameter": 6778,
+          "gravity": "0.4",
+          "magnetic_field_strong": false,
+          "owner_id": 1
+        },
+        {
+          "id": 5,
+          "planet": "jupiter",
+          "class": "gaseous",
+          "diameter": 139822,
+          "gravity": "2.5",
+          "magnetic_field_strong": true,
+          "owner_id": 1
         }
       ];
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/planets')
         .expect('Content-Type', /json/)
         .expect(200);
 
